5ef3a91eedeff77def855fd767231134df0c6ed7
guillermo <guillermo@GAK-VirtualBox>
2014-09-08 16:24:06 -0300

    Front End editor

--- TreaProject/dynamicForms/urls.py
+++ TreaProject/dynamicForms/urls.py
@@ -6,12 +6,16 @@ Created on 30/8/2014
 from django.conf.urls import patterns, url
 from rest_framework.urlpatterns import format_suffix_patterns
 from dynamicForms import views 
+from django.contrib.messages.tests.urls import TEMPLATE
+from django.views.generic.base import TemplateView
+from rest_framework.urls import template_name
 
 urlpatterns = patterns('dynamicForms.views',
     url(r'(?P<pk>[0-9]+)/$', views.FormDetail.as_view()),
-    url(r'$', views.FormList.as_view()),
+  #  url(r'$', views.FormList.as_view()),
     url(r'^users/$', views.UserList.as_view()),
     url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),
+    url(r'^index.html', TemplateView.as_view(template_name= 'mainPage.html')),
 )
 
 urlpatterns = format_suffix_patterns(urlpatterns)
