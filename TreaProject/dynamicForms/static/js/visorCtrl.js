a081d04eb1ba85c72d59f4131df0e4e96b614313
nahu <nahu@chiriguare>
2014-09-20 15:58:25 -0300

    Carga de título en visor

--- TreaProject/dynamicForms/static/js/visorCtrl.js
+++ TreaProject/dynamicForms/static/js/visorCtrl.js
@@ -15,25 +15,27 @@
          * To get the form the slug is catched form the path.
          * This should be handled by $routerprovider
          */
-        visor.path = location.pathname.match(/\/visor\/(.*)/)[1];
+        //FIXME: corregir la manera de obtener el slug
+        visor.slug = location.pathname.match(/\/visor\/(.*)/)[1];
         visor.versionNum = 1;
-            //Load Form
-        $http.get('/dynamicForms/visor/publishVersion/'+visor.path)
+        visor.title = '';
+            // Load Form
+        $http.get('/dynamicForms/form/'+visor.slug)
          .success(function(data){
-            visor.version = data;
-             visor.questions = JSON.parse(data.json).Fields;
-            /*    //Load version
-            $http.get('version/'+visor.path+'/'+visor.versionNum)
-            .success(function(data){
-                visor.version = data;
-                visor.questions = JSON.parse(data.json).Fields;
-            })
-            .error(function(data, status, headers, config){
-                alert('error cargando version: ' + status);
-            })*/
+             visor.title = data.title;
+             visor.form = data;
          })
          .error(function(data, status, headers, config){
              alert('error cargando formulario: ' + status);
+         })
+            // Load Version
+        $http.get('/dynamicForms/visor/publishVersion/'+visor.slug)
+         .success(function(data){
+            visor.version = data;
+             visor.questions = JSON.parse(data.json).Fields;
+         })
+         .error(function(data, status, headers, config){
+             alert('error cargando las preguntas: ' + status);
          })
     }]);
 })();
