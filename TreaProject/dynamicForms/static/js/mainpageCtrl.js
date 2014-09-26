88070ac2f6cf19f4bd37d2eb252ae7e6fecbd31e
mats-claassen <mathiasclaassen7@gmail.com>
2014-09-26 19:24:51 -0300

    detalles front respuestas

--- TreaProject/dynamicForms/static/js/mainpageCtrl.js
+++ TreaProject/dynamicForms/static/js/mainpageCtrl.js
@@ -17,27 +17,13 @@
         mainPage.slug = pattern[1];
         mainPage.ver = pattern[2];
 
-        mainPage.json = [
-            {
-                "entry_time": "2014-09-25",
-                "fields": [
-                    "dsdsds : nnnnnnnnnnnnnnnn"
-                ]
-            },
-            {
-                "entry_time": "2014-09-25",
-                "fields": [
-                    "dsdsds : mmmmmmmmmmm"
-                ]
-            }
-        ];
-/*
-        $http.get('/dynamicForms/responses/'+mainPage.slug+'/'mainPage.ver)
+
+        $http.get('/dynamicForms/responses/'+mainPage.slug+'/'+ mainPage.ver+ '/d')
             .success(function(data){
                 mainPage.json = data;
             })
             .error(function(data, status, headers, config){
                 alert('error cargando respuestas: ' + status);
             })
-*/    }]);
+    }]);
 })();
