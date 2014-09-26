1bc1acb8d7693c9da28e5c98f7eec1fecec86301
nahu <nahu@chiriguare>
2014-09-26 18:34:54 -0300

    Historia a media

--- TreaProject/dynamicForms/static/js/app.js
+++ TreaProject/dynamicForms/static/js/app.js
@@ -5,7 +5,42 @@
     * Module dynamicFormsFramework
     * This module encapsulates the logic that will handle the form.
     */
-    var app = angular.module('dynamicFormsFramework', []);
-          
+    var app = angular.module('dynamicFormsFramework', ['ui.router'])
+        .config(function($stateProvider, $urlRouterProvider) {
+    
+            $stateProvider
+	
+            // route to show our basic form (/form)
+            .state('visor', {
+                url: '/dynamicForms/visor/:formSlug/',
+                //templateUrl: 'form.html',
+                //controller: 'formController'
+                controller: function($scope, $stateParams) {
+                    // get the id
+                    $scope.formSlug = $stateParams.formSlug;
+                }
+            });
+
+            // nested states 
+            // each of these sections will have their own view
+            // url will be nested (/form/profile)
+            /*.state('form.profile', {
+                url: '/profile',
+                templateUrl: 'form-profile.html'
+            })
+
+            // url will be /form/interests
+            .state('form.interests', {
+                url: '/interests',
+                templateUrl: 'form-interests.html'
+            })
+
+            // url will be /form/payment
+            .state('form.payment', {
+                url: '/payment',
+                templateUrl: 'form-payment.html'
+            });*/
+        });
+    
 })();
 
--- TreaProject/dynamicForms/static/js/visorCtrl.js
+++ TreaProject/dynamicForms/static/js/visorCtrl.js
@@ -2,53 +2,53 @@
 
 (function () {
 	
-    var app = angular.module('dynamicFormsFramework');
+    var app = angular.module('dynamicFormsFramework')
     
-    /*
-     * The VisorCtrl holds the logic to display, validate and submit the form.
-     */
-    app.controller('VisorCtrl', ['$scope','$http', function ($scope, $http) {
-
-        var visor = this;
-        
         /*
-         * To get the form the slug is catched form the path.
-         * This should be handled by $routerprovider
+         * The VisorCtrl holds the logic to display, validate and submit the form.
          */
-        //FIXME: corregir la manera de obtener el slug
-        visor.slug = location.pathname.match(/\/visor\/(.*)/)[1];
-            // Load Form
-        $http.get('/dynamicForms/form/'+visor.slug)
-            .success(function(data){
-                visor.title = data.title;
-                visor.form = data;
-            })
-            .error(function(data, status, headers, config){
-                alert('error cargando formulario: ' + status);
-            })
-            // Load Version
-        visor.questions = [];
-        $http.get('/dynamicForms/visor/publishVersion/'+visor.slug)
-            .success(function(data){
-                visor.version = data;
-                visor.pages = JSON.parse(data.json).pages;
-                for (var i=0; i<visor.pages.length; i++) {
-                    visor.questions = visor.questions.concat(visor.pages[i].fields);
-                };
-            })
-            .error(function(data, status, headers, config){
-                alert('error cargando las preguntas: ' + status);
-            })
-        
-        visor.save = function(){
-        $http.post('/dynamicForms/visorPub/'+visor.form.slug+'/submit/',visor.questions)
-            .success( function(data, status, headers, config){
-                
-            })
-            .error(function(data, status, headers, config) {
-                alert('Error guardando las respuestas: ' + status);
-            });
-        };
-        
-    }]);
+        .controller('VisorCtrl', ['$scope','$http', function ($scope, $http) {
+
+            var visor = this;
+
+            /*
+             * To get the form the slug is catched form the path.
+             * This should be handled by $routerprovider
+             */
+            //FIXME: corregir la manera de obtener el slug
+            visor.slug = location.pathname.match(/\/visor\/(.*)/)[1];
+                // Load Form
+            $http.get('/dynamicForms/form/'+visor.slug)
+                .success(function(data){
+                    visor.title = data.title;
+                    visor.form = data;
+                })
+                .error(function(data, status, headers, config){
+                    alert('error cargando formulario: ' + status);
+                })
+                // Load Version
+            visor.questions = [];
+            $http.get('/dynamicForms/visor/publishVersion/'+visor.slug)
+                .success(function(data){
+                    visor.version = data;
+                    visor.pages = JSON.parse(data.json).pages;
+                    for (var i=0; i<visor.pages.length; i++) {
+                        visor.questions = visor.questions.concat(visor.pages[i].fields);
+                    };
+                })
+                .error(function(data, status, headers, config){
+                    alert('error cargando las preguntas: ' + status);
+                });
+
+            visor.save = function(){
+                $http.post('/dynamicForms/visorPub/'+visor.form.slug+'/submit/',visor.questions)
+                    .success( function(data, status, headers, config){
+
+                    })
+                    .error(function(data, status, headers, config) {
+                        alert('Error guardando las respuestas: ' + status);
+                    });
+            };
+
+        }]);
 })();
