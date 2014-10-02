8f32a7feaf98e77dea27d2db7564efa46f4c4e37
nahu <nahu@chiriguare>
2014-10-02 18:19:13 -0300

    Deshabilitación html5 para corregir error en editor

--- TreaProject/dynamicForms/static/js/app.js
+++ TreaProject/dynamicForms/static/js/app.js
@@ -7,7 +7,7 @@
     */
     var app = angular.module('dynamicFormsFramework', ['ui.sortable'])
     .config(['$locationProvider', function ($locationProvider) {
-        $locationProvider.html5Mode(true).hashPrefix('!');
+        //$locationProvider.html5Mode(true).hashPrefix('!');
     }]);
     
 })();
