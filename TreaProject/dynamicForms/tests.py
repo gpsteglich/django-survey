c31af7add88d27054f1225e4a0023b4a0b7878e5
valentin <valentin@Mipc>
2014-10-06 12:01:28 -0200

    Test

--- TreaProject/dynamicForms/tests.py
+++ TreaProject/dynamicForms/tests.py
@@ -1,98 +1,91 @@
 from django.test import TestCase
 # Create your tests here.
-from dynamicForms import models
+
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
 from django.test.client import Client
 import datetime;
+
 from dynamicForms import login
+from dynamicForms.fields import PUBLISHED, DRAFT
+from dynamicForms import models
 
 
 class FormTestCase(TestCase):
     
     def setUp(self):
         
-        f1 = models.Form.objects.create(title = "new form1", 
-                                        publish_date = datetime.datetime.now(),
-                                        expiry_date = datetime.datetime.now(),
-                                        owner_id = 1)
-        f2 = models.Form.objects.create(title = "new form2", 
-                                        publish_date = datetime.datetime.now(),
-                                        expiry_date = datetime.datetime.now(),
-                                        owner_id = 1)
-        f3 = models.Form.objects.create(title = "new form3", 
-                                        publish_date = datetime.datetime.now(),
-                                        expiry_date = datetime.datetime.now(),
-                                        owner_id = 1)
-        f4 = models.Form.objects.create(title = "new form4", 
-                                        publish_date = datetime.datetime.now(),
-                                        expiry_date = datetime.datetime.now(),
-                                        owner_id = 1)
-        f5 = models.Form.objects.create(title = "new form5", 
-                                        publish_date = datetime.datetime.now(),
-                                        expiry_date = datetime.datetime.now(),
-                                        owner_id = 1)
+        #testing client
+        self.c = Client()
+       
+        #testing user
+        self.user = User.objects.create_user('user', 'user@mail.com', 'password')
         
-        self.user = User.objects.create_user(
-            username='user', email='user@Test', password='user')
+        #login user
+        self.c.login(username='user', password='password')
+        
+        #create some testing forms
+        self.f1 = models.Form.objects.create(title = "new form1", owner= user)
+        self.v1 = models.Version.objects.create(json='{"prueba":"valor"}', form=f1)
+        self.f2 = models.Form.objects.create(title = "new form2", owner= user)
+        self.v2 = models.Version.objects.create(json='{"prueba":"valor"}', form=f2)
+                                      
+    def test_new_version_form(self):
+        
+        #create-new-version-for-form-f1
+        self.c.get("/dynamicForms/version/"+self.f1.id+"/"+self.v1.number+"/new", follow=True)
+        
+        #check-if-new-version-was-created
+        new_version = models.Version.objects.get(number=self.v1.number+1)
+        
+        #check-if-new-version's-data-is-correct
+        self.assertEqual(new_version.form, self.f1)   
+        self.assertEqual(new_version.status, DRAFT)
+        self.assertEqual(new_version.json, self.v1.json)
+    
+  
+    def test_duplicate_form(self):
+        
+        #duplicate-form-f1
+        self.c.get("/dynamicForms/version/"+self.f1.id+"/"+self.v1.number+"/duplicate", follow=True)
+        
+        #check-if-form-was-correctly-duplicated
+        f1_duplicated = models.Form.objects.get(title=self.f1.title+"(duplicated)")
+        
+        #check-if-its+data-is-correct
+        versions = models.Version.objects.filter(form=f1_duplicated)
+             
+        self.assertEqual(len(versions), 1)
+        v = versions[0]
+        self.assertEqual(v.status, DRAFT)
+        self.assertEqual(v.json, self.v1.json) 
         
         
-    def test_create(self):
-        
-        f1 = models.Form.objects.get(title = "new form1")
-        self.assertEqual(f1.title, "new form1")   
-        self.assertEqual(f1.slug, "new-form1")
-        
-        
-    def test_slug_exists(self):
-         
-        try:
-            f1 = models.Form.objects.create(title = "new fórm1", 
-                                            publish_date = datetime.datetime.now(),
-                                            expiry_date = datetime.datetime.now(),
-                                            owner_id = 1)    
-        except Exception:
-         
-             print ("OK: Creation of new form not allowed because generated slug already exists db")
-         #f2 = models.Form.objects.get(title = "new form1")
-        
-         
     def test_delete_form(self):
-                 
-        try: 
-            f1 = models.Form.objects.get(title = "new form6")
-        except: 
-            print("OK: new form6 does not exists because it has never been created")
-            
-        f1 = models.Form.objects.get(title = "new form4")
-        f1.delete()
         
+        #delete-form-f1
+        self.c.get("/dynamicForms/form/delete/"+self.f2.id, follow=True)
+        
+        #check-if-form-was-correctly-deleted
         try:
-            f1 = models.Form.objects.get(title = "new form4")
+            f2 = models.Form.objects.get(title="new form2" )
         except:
-            print("OK: new form4 has been deleted")
+            print("OK: new f1 has been deleted")   
+
+    def test_delete_version(self):
+        
+        #delete-version1-form-f1
+        self.c.get("/dynamicForms/version/delete/"+self.f1.id+"/"+self.v1.number, follow=True)
+        
+        #check-if-form-was-correctly-deleted
+        try:
+            f1 = models.Form.objects.get(title="new form1" )
+        except:
+            print("OK: new f1 has been deleted")
             
-        f1 = models.Form.objects.get(title = "new form2")
-        self.assertEqual(f1.title, "new form2")
-        self.assertEqual(f1.slug, "new-form2")
-        entry_list = list(models.Form.objects.all())
-        count = entry_list.__len__()
-        self.assertEqual(count,4)
-        
-         
-    def test_count_forms(self):     
-         
-         entry_list = list(models.Form.objects.all())
-         count = entry_list.__len__()
-         self.assertEqual(count,5)
-        
-         
-    def test_fall_login(self):
-        
-        c = Client()
-        response = c.post('/dynamicForms/login/', {'username': 'user', 'password': 'user'}, follow = True)
-        self.assertEqual(response.status_code, 200)
-        response = c.post('/dynamicForms/login/', {'username': 'user2', 'password': 'user2'}, follow = True)
-        self.assertEqual(response.status_code, 200)
-        
-        
+        try:
+            v1 = models.Version.objects.get(number=self.v1.number)
+        except:
+            print("OK: new v1 has been deleted")         
+    
+          
