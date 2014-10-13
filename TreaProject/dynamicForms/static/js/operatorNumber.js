b21fc03f64c2209a1a0d6147d686252cdbaf0dda
nahu <nahu@chiriguare>
2014-10-13 19:02:47 -0200

    Arreglo operadores de number

--- TreaProject/dynamicForms/static/js/operatorNumber.js
+++ TreaProject/dynamicForms/static/js/operatorNumber.js
@@ -7,26 +7,29 @@ OperatorNumber.prototype.constructor = OperatorNumber;
 
 OperatorNumber.greater_than = function(a, b){
 	return (a > b);
-}
+};
 
 OperatorNumber.greater_than_or_equal = function(a, b){
 	return (a >= b);
-}
+};
 
 OperatorNumber.equal = function(a, b){
 	return (a == b);
-}
+};
 
 OperatorNumber.not_equal = function(a, b){
 	return (a != b);
-}
+};
 
 OperatorNumber.less_than_or_equal = function(a, b){
 	return (a <= b);
-}
+};
 
 OperatorNumber.less_than = function(a, b){
-	return (a < b);
-}
+	console.log('a: ' +a + ' b: '+b);
+	aInt = parseInt(a,10);
+	bInt = parseInt(b,10);
+	return (aInt<bInt);
+};
 
 operatorFactory.registerOperator('number', OperatorNumber);
