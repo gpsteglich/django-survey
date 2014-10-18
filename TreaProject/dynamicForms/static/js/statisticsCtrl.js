86620281895abe46e834a6d8299fae1738015764
pperera611 <pperera611@gmail.com>
2014-10-18 16:24:36 -0200

    Arreglada la importacion de js. Grafíca lo que esta en el controlador de estadistica harcodeado

--- TreaProject/dynamicForms/static/js/statisticsCtrl.js
+++ TreaProject/dynamicForms/static/js/statisticsCtrl.js
@@ -9,81 +9,51 @@
      */    
     app.controller('statisticsCtrl', function ($scope, $http, $location, $window) {
     	
-    	var stat = this;
-        
-        stat.mensaje = "se comunican";
-        
-        alert('entro al controlador');
-        
-        stat.config = {
-                  title: '',
-                  tooltips: true,
-                  labels: false,
-                  mouseover: function() {},
-                  mouseout: function() {},
-                  click: function() {},
-                  legend: {
-                    display: true,
-                    //could be 'left, right'
-                    position: 'left'
-                  },
-                  innerRadius: 0, // applicable on pieCharts, can be a percentage like '50%'
-                  lineLegend: 'lineEnd' // can be also 'traditional'
-                    }
-        
-        stat.data = {
-                  "series": [
-                    "Sales",
-                    "Income",
-                    "Expense"
-                  ],
-                  "data": [
-                    {
-                      "x": "Computers",
-                      "y": [
-                        54,
-                        0,
-                        879
-                      ],
-                      "tooltip": "This is a tooltip"
-                    }
-                  ]
-                }
+    	//var stat = this;
         /*
-        
-        mainPage.formSlugParam = ($location.search()).form;
-        mainPage.versionIdParam = ($location.search()).ver;
-        mainPage.orders = [
-            {name: "Id", value: "id"},
-            {name: "Owner", value: "owner"},
-            {name: "Title", value: "title"},
-            //{name: "Publish Date", value: "publish_date"},
-        ]
-
-        mainPage.selectascdsc = function(ascdsc){
-            mainPage.ascdsc = ascdsc;
-        }
-
-        mainPage.url = function(){
-            var parser = $location.absUrl();
-            var arr = parser.split('/');
-            var crit = arr[arr.length - 3];
-            var sent = arr[arr.length - 2];
-            return ([crit, sent]);
-        }
-
-     
-    
-        mainPage.getResponses = function(){
-            $http.get('/dynamicForms/responses/'+mainPage.formSlugParam+'/'+ mainPage.versionIdParam+'/')
-            .success(function(data){
-                mainPage.json = data;
-            })
-            .error(function(data, status, headers, config){
-                alert('Error loading form data: ' + status);
-            })
-        }
+        $http.get('/dynamicForms/statistics/'+)
+                .success(function(data){
+                    visor.version = data;
+                    visor.pages = JSON.parse(data.json).pages;
+                    visor.logic = JSON.parse(data.json).logic;
+                    visor.initialiceConditions();
+                    visor.changePage(0);
+                })
+                .error(function(data, status, headers, config){
+                    alert('error loading statistics: ' + status);
+                });
         */
-        
-    }]);
+        $scope.config = {
+            title: 'Products',
+            tooltips: true,
+            labels: false,
+            mouseover: function() {},
+            mouseout: function() {},
+            click: function() {},
+            legend: {
+                display: true,
+                //could be 'left, right'
+                position: 'right'
+            }
+        };
+
+       $scope.data = {
+            series: ['Sales', 'Income', 'Expense', 'Laptops', 'Keyboards'],
+            data: [{
+              x: "Laptops",
+              y: [100, 500, 0],
+              tooltip: "this is tooltip"
+            }, {
+              x: "Desktops",
+              y: [300, 100, 100]
+            }, {
+              x: "Mobiles",
+              y: [351]
+            }, {
+              x: "Tablets",
+              y: [54, 0, 879]
+            }]
+          };
+
+    });
 })();
