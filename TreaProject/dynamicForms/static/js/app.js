e07a02fd9ffc8553a56622e4cfcbbaafdac14089
nahu <nahu@chiriguare>
2014-10-19 21:40:46 -0200

    Corrección csrf token

--- TreaProject/dynamicForms/static/js/app.js
+++ TreaProject/dynamicForms/static/js/app.js
@@ -6,8 +6,27 @@
     * This module encapsulates the logic that will handle the form.
     */
     var app = angular.module('dynamicFormsFramework', ['ui.sortable','ui.bootstrap','checklist-model'])
-    .config(['$locationProvider', function ($locationProvider) {
-        //$locationProvider.html5Mode(true).hashPrefix('!');
+    .config(['$locationProvider', '$httpProvider', function ($locationProvider, $httpProvider) {
+
+        $httpProvider.defaults.xsrfCookieName = 'csrftoken';
+		$httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
+
     }]);
 })();
 
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie != '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = jQuery.trim(cookies[i]);
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+var csrftoken = getCookie('csrftoken');
--- TreaProject/dynamicForms/static/js/appVisor.js
+++ TreaProject/dynamicForms/static/js/appVisor.js
@@ -6,8 +6,12 @@
     * This module encapsulates the logic that will handle the form.
     */
     var app = angular.module('dynamicFormsFramework', ['ui.bootstrap','checklist-model'])
-    .config(['$locationProvider', function ($locationProvider) {
+    .config(['$locationProvider','$httpProvider', function ($locationProvider, $httpProvider) {
+        
         $locationProvider.html5Mode(true);
+        $httpProvider.defaults.xsrfCookieName = 'csrftoken';
+		$httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
+
     }]);
     
 })();
