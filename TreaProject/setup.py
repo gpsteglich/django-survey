ba3cc2a3ca92f69fbe327ff0be03b7f3b7b9fcc1
mats-claassen <mathiasclaassen7@gmail.com>
2014-10-23 09:47:25 -0200

    setup.py

---
+++ TreaProject/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+ 
+setup(
+    name = 'dynamicForms',
+    packages = ['dynamicForms'],
+    version = '0.0.1',
+    description = 'Creates surveys with multipaging and multipath support.',
+    author='PythonWarriors',
+    author_email='info@trea.uy',
+    url='https://github.com/trea-uy/django-survey',
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.4',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Operating System :: OS Independent',
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Information Technology',
+        'Topic :: Internet :: WWW/HTTP :: WSGI :: Application'
+    ]
+)
