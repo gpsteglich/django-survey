2e662d4dac82d552f1fabf02651249ab99e3f8e8
guillermo <guillermo@GAK-VirtualBox>
2014-10-26 19:04:40 -0200

    listo las exportaciones a pdf

--- TreaProject/dynamicForms/static/js/statisticsCtrl.js
+++ TreaProject/dynamicForms/static/js/statisticsCtrl.js
@@ -101,6 +101,31 @@
      
         stat.getF();
        // console.log(stat.values);
+
+       $scope.createPDF = function(){
+                            // var doc = new jsPDF();
+                            // doc.text(20, 20, 'Hello world!');
+                            // doc.text(20, 30, 'This is client-side Javascript, pumping out a PDF.');
+                            // doc.addPage('a6','l');
+                            // doc.text(20, 20, 'Do you like that?');
+                            // doc.save();
+                           
+                            var data = [],fontSize = 12, height = 0,doc;
+                            doc = new jsPDF('p', 'pt', 'a4', true);
+                            doc.setFont("courier", "normal");
+                            doc.setFontSize(fontSize);
+                            for (var insert = 0; insert <= 20; insert++) {
+                                data.push({
+                                    "name" : "jspdf plugin",
+                                    "version" : insert,
+                                    "author" : "Guillermo Kuster",
+                                    "Designation" : "AngularJs Developer"
+                                });
+                            }
+                            height = doc.drawTable(data, {xstart:10,ystart:10,tablestart:70,marginleft:50});      
+                            doc.save("statistics.pdf");
+                         
+                           }
         
     $scope.chart_types = [
         'pie',
