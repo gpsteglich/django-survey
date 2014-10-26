4382c89cb48a5f08e83c0c839532dfb6d1e59ef9
nahu <nahu@chiriguare>
2014-10-26 20:27:08 -0200

    Validators y arreglos

---
+++ TreaProject/dynamicForms/static/js/validators/TextField.js
@@ -0,0 +1,22 @@
+function TextField() {
+	
+}
+
+/*
+ * Validates the field with the values of the attributes of
+ * the element.
+ */
+TextField.validate = function(value, attrs){
+	/*
+	 * attrs = a dictionary of attributes on the element
+	 */
+	var ok = true;
+	/* Validate example*/
+	//ok = 'hello world' == value;
+	//ok &= value.length <= attrs.ngMaxlength;
+	
+	return (ok);
+};
+
+// Register field constructor in Factory
+validatorFactory.registerValidator(TextField.name, TextField);
