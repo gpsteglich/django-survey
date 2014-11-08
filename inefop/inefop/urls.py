5fb6b0e142b413138c40e424f37233a84a610e63
mats-claassen <mathiasclaassen7@gmail.com>
2014-11-08 17:00:45 -0200

    Plugin despaginado

--- inefop/inefop/urls.py
+++ inefop/inefop/urls.py
@@ -4,9 +4,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'inefop.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
     url(r'^forms/', include('formularios.urls')),
     url(r'^inefop/', include('dynamicForms.urls'), name='base'),
     url(r'^admin/', include(admin.site.urls)),
