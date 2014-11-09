fc1fc4a3f2497929de9bef9dfad0ae46e122110b
nahu <nahu@chiriguare>
2014-11-09 19:38:00 -0200

    test main page

--- TreaProject/dynamicForms/testing/mainPage_test.js
+++ TreaProject/dynamicForms/testing/mainPage_test.js
@@ -3,34 +3,35 @@ describe("MainPage Testing", function() {
     beforeEach(angular.mock.module('dynamicFormsFrameworkAdmin'));
     beforeEach(inject(function ($rootScope, $controller, _$location_, _$httpBackend_) {
         $location = _$location_;
+        $location.url('/id/asc/');
         scope = $rootScope.$new();
         createController = function() {
             return $controller('MainPageCtrl', {
-                '$scope': scope
+                '$scope': scope,
+                '$location' : $location,
             });
         };
     }));
     
     it("Testing main modes.", inject(function($controller,$rootScope) {
         var ctrl = createController();
-        
         var order = [
             {name: "Id", value: "id"},
             {name: "Owner", value: "owner"},
             {name: "Title", value: "title"},
         ];
-        console.log('orders '+order.toSource());
-        console.log('ctrl.orders '+ctrl.orders.toSource());
-        console.log(order == ctrl.orders);
-        order = angular.copy(ctrl.orders);
-        expect(ctrl.orders).toBe(order);
-        /*
-        expect(scope.isPreviewMode()).toBe(false);
-        scope.slug = undefined;
-        scope.formIdParam = 1;
-        scope.versionIdParam = 1;
-        expect(scope.isVisorMode()).toBe(false);
-        expect(scope.isPreviewMode()).toBe(true);*/
+        expect(ctrl.orders).toEqual(order);
+        var param = 'asc';
+        ctrl.selectascdsc(param);
+        expect(ctrl.ascdsc).toEqual(param);
+        param = 'dsc';
+        ctrl.selectascdsc(param);
+        expect(ctrl.ascdsc).toEqual(param);
+        expect(ctrl.actualOrder()).toEqual(order[0]);
+        $location.url('/owner/asc/');
+        expect(ctrl.actualOrder()).toEqual(order[1]);
+        $location.url('/title/asc/');
+        expect(ctrl.actualOrder()).toEqual(order[2]);
     }));
 
 });
