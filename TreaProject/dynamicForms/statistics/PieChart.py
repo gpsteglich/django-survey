65ddc064b1c326006725481b883994f0c26f1fbb
valentin <valentin@Mipc>
2014-11-15 15:42:44 -0200

    pronto pa testing el pdf en back

--- TreaProject/dynamicForms/statistics/PieChart.py
+++ TreaProject/dynamicForms/statistics/PieChart.py
@@ -1,13 +1,31 @@
 from reportlab.graphics.charts.piecharts import Pie
 from reportlab.graphics.charts.legends import Legend
 from reportlab.graphics.shapes import Drawing 
+from reportlab.lib.colors import Color, HexColor
+
 
 class PieChart(Drawing):
     
     def __init__(self, data, labels):
         
         super(PieChart, self).__init__(400,200)
-         
+        
+        colors = [  
+            HexColor("#0000e5"),  
+            HexColor("#ff0011"),
+            HexColor("#800000"),
+            HexColor("#e05897"),   
+            HexColor("#a08ff7"),  
+            HexColor("#8f8ff5"),  
+            HexColor("#c7c7fa"),  
+            HexColor("#800000"),  
+            HexColor("#eb8585"),   
+            HexColor("#d60a0a"),  
+            HexColor("#ffff00"),
+            HexColor("#1f1feb"),   
+        ]  
+        
+        #create pie chart 
         pieChart = Pie()
         pieChart.x = 40
         pieChart.y = 30
@@ -19,6 +37,19 @@ class PieChart(Drawing):
         for d in data:
             pieChart.labels.append(str(d))
             
-        self.add(pieChart, "pie chart")
-        
+        #create legend
         legend = Legend()
+        legend.x = 380
+        legend.y = 60  
+        legend.boxAnchor           = 'se'  
+        legend.subCols[1].align    = 'right'
+        legend.colorNamePairs = []
+           
+        len_data = len(data) 
+        for i in range(0,len_data):
+            pieChart.slices[i].fillColor = colors[i]
+            legend.colorNamePairs.append((colors[i],labels[i]))
+            
+        
+        self.add(pieChart, "pie chart")       
+        self.add(legend, "legend")
