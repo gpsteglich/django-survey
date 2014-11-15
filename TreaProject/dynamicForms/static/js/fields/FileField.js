34ffb0c0f469cac679a8ceec75486a38695122b5
federico <federico@federico>
2014-11-15 20:22:16 -0200

    FileField.js agregado.

---
+++ TreaProject/dynamicForms/static/js/fields/FileField.js
@@ -0,0 +1,13 @@
+function FileField() {
+	
+}
+
+FileField.buildField = function(){
+	var field = FieldBase.buildField(this);
+	field.field_type = 'FileField';	
+    field.validations = {};
+	return (field);
+};
+
+// Register field constructor in Factory
+fieldFactory.registerField('FileField', FileField);
