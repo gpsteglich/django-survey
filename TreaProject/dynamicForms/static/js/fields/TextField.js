8f00a4c9f3393eb0540858391b7de4336e0f7e6b
mats-claassen <mathiasclaassen7@gmail.com>
2014-11-15 10:58:26 -0200

    vvh

--- TreaProject/dynamicForms/static/js/fields/TextField.js
+++ TreaProject/dynamicForms/static/js/fields/TextField.js
@@ -4,7 +4,7 @@ function TextField() {
 
 TextField.buildField = function(){
 	var field = FieldBase.buildField(this);
-	field.field_type = TextField.name;
+	field.field_type = "TextField";
 	field.validations = {
         max_len_text: 255,
     };
@@ -12,4 +12,4 @@ TextField.buildField = function(){
 };
 
 // Register field constructor in Factory
-fieldFactory.registerField(TextField.name, TextField);
+fieldFactory.registerField("TextField", TextField);
