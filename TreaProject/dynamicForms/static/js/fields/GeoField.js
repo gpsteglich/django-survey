26fa904a0d4dd4fc4f10e1970c531852e89a5b76
nahu <nahu@chiriguare>
2014-11-16 13:10:13 -0200

    Correcion codigo

--- TreaProject/dynamicForms/static/js/fields/GeoField.js
+++ TreaProject/dynamicForms/static/js/fields/GeoField.js
@@ -7,7 +7,7 @@ GeoField.buildField = function(){
     var field = FieldBase.buildField(this);
     
 	field.field_type = 'GeoField';
-    field.mapzoom =8,
+    field.mapzoom = 8,
     field.mapXY = {
         latitude: -34.806777135903424,
         longitude: -56.164398487890594
