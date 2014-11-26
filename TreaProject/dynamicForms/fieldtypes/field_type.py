14f079a76ed40e16cc3cf5d4c4ae82a97d6c3943
mats-claassen <mathiasclaassen7@gmail.com>
2014-11-26 11:48:22 -0200

    deleting others

--- TreaProject/dynamicForms/fieldtypes/field_type.py
+++
@@ -1,6 +0,0 @@
-from django.conf import settings
-
-
-def on_startup():
-    for file in settings.FIELD_FILES:
-        __import__(file, fromlist=[""])
--- TreaProject/dynamicForms/signals.py
+++
@@ -1,6 +0,0 @@
-from django.db.models.signals import post_save
-from django.dispatch import receiver, Signal
-from dynamicForms.models import Form, Version
-
-
-modified_logic = Signal(providing_args=["sent_data"])
