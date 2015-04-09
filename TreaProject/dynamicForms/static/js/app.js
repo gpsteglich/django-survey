652b98ce03ecaa43b490c076eb20a34cba9bdd92
vmartinez <vmartinez@trea.uy>
2015-04-09 11:56:04 -0300

    Resource module import

--- TreaProject/dynamicForms/static/js/app.js
+++ TreaProject/dynamicForms/static/js/app.js
@@ -5,28 +5,33 @@
     * Module dynamicFormsFramework
     * This module encapsulates the logic that will handle the form.
     */
-    var app = angular.module('dynamicFormsFrameworkAdmin', ['ui.sortable','ui.bootstrap','checklist-model','angularCharts'])
+    var app = angular.module('dynamicFormsFrameworkAdmin', ['ui.sortable','ui.bootstrap','checklist-model','angularCharts', 'ngResource'])
     .config(['$locationProvider', '$httpProvider', function ($locationProvider, $httpProvider) {
 
         $httpProvider.defaults.xsrfCookieName = 'csrftoken';
-		$httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
-     
+        $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
+
     }]);
-    
+
+    app.config(['$resourceProvider', function($resourceProvider) {
+       // Don't strip trailing slashes from calculated URLs
+       $resourceProvider.defaults.stripTrailingSlashes = false;
+    }]);
+
     app.directive('ngConfirmClick', [function(){
-		return {
-	  		priority: -1,
-	  		restrict: 'A',
-	  		link: function(scope, element, attrs){
-	    		element.bind('click', function(e){
-	      			var message = attrs.ngConfirmClick;
-	      			if(message && !confirm(message)){
-	        			e.stopImmediatePropagation();
-	        			e.preventDefault();
-	      			}
-	    		});
-	  		}
-		}
-	}]);
+        return {
+            priority: -1,
+            restrict: 'A',
+            link: function(scope, element, attrs){
+                element.bind('click', function(e){
+                    var message = attrs.ngConfirmClick;
+                    if(message && !confirm(message)){
+                        e.stopImmediatePropagation();
+                        e.preventDefault();
+                    }
+                });
+            }
+        }
+    }]);
 
 })();
